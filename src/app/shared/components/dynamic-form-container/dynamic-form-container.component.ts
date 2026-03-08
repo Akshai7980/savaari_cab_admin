8f52d67a0ed2ae7b88e29129fa8e47f60ddddea5
Akshai7980 <akshay7980@gmail.com>
2026-03-08 15:58:10 +0530

    feat: Introduce `fieldClick` output, `readonly` form field property, `PickerManagerService`, and update driver booking form configurations.

--- src/app/shared/components/dynamic-form-container/dynamic-form-container.component.ts
+++ src/app/shared/components/dynamic-form-container/dynamic-form-container.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { FormConfig, FormSectionConfig } from 'src/app/core/models/form-field.model';
+import { FormConfig, FormFieldConfig, FormSectionConfig } from 'src/app/core/models/form-field.model';
 import { DynamicFormComponent } from '../dynamic-form/dynamic-form.component';
 
 @Component({
@@ -14,6 +14,7 @@ import { DynamicFormComponent } from '../dynamic-form/dynamic-form.component';
 export class DynamicFormContainerComponent implements OnInit {
     @Input() config!: FormConfig;
     @Input() formGroup!: FormGroup;
+    @Output() fieldClick = new EventEmitter<{ fieldID: string; config: FormFieldConfig }>();
 
     ngOnInit(): void {
         if (!this.formGroup && this.config) {
